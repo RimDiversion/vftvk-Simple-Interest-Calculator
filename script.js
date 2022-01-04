ae0aa06efcdb678fd25de730cde928d3736179f1
RimDiversion <danchristensen9@gmail.com>
2022-01-04 14:57:28 -0500

    testing javascript

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    
+    document.getElementById("result").innerHTML= "If you deposit " + principal + ",/n"  
+        + "at an interest rate of " + rate + "%./n"
+        + "You will recieve an amount of " + (interest + principal) + ",/n"
+        + "in the year " + year   
 }
         
 function updateRate()
