bdc2f45a5332ff1d7d183a5d9dcc485c61603d9c
RimDiversion <danchristensen9@gmail.com>
2022-01-04 16:25:31 -0500

    minor fix

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    if (parseInt(principal) <= 0) {
+    if (principal <= 0) {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
-    };
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    var total = parseInt(principal, 10) + interest;
-    document.getElementById("result2").innerHTML= "If you deposit " + principal + ",<br>"  
-        + "at an interest rate of " + rate + "%.<br>"
-        + "You will recieve an amount of " + total + ",<br>"
-        + "in the year " + year;
-    document.getElementById("result").innerText= interest;   
+    } else {
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        var total = parseInt(principal, 10) + interest;
+        document.getElementById("result2").innerHTML= "Summary: " + "<br>" 
+            + "If you deposit " + principal + ",<br>"  
+            + "at an interest rate of " + rate + "%.<br>"
+            + "You will recieve an amount of " + total + ",<br>"
+            + "in the year " + year;
+        document.getElementById("result").innerText= "Total interest: " + interest;
+    };   
 }
 
 // updates the interest rate span upon change
