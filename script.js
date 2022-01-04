03feaa0d5da82efffca868c8a1693eb9b1d377e9
RimDiversion <danchristensen9@gmail.com>
2022-01-04 15:39:01 -0500

    more updates and fixes

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// This function pulls needed values and validates a correct pricipal was entered
 function compute()
 {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    //checks is pricipal is greater than 0
     if (principal <= 0) {
         alert("Enter a positive number");
         document.getElementById("pricipal").focus();
@@ -10,13 +12,14 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     var total = parceInt(principal) + parceInt(interest)
-    document.getElementById("result2").innerHTML= "If you deposit " + principal + ",\n"  
-        + "at an interest rate of " + rate + "%.\n"
-        + "You will recieve an amount of " + total + ",\n"
+    document.getElementById("result2").innerHTML= "If you deposit " + principal + ",<br>"  
+        + "at an interest rate of " + rate + "%.<br>"
+        + "You will recieve an amount of " + total + ",<br>"
         + "in the year " + year;
     document.getElementById("result").innerText= interest   
 }
-        
+
+// updates the interest rate span upon change
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
