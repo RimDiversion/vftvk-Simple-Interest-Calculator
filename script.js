e7dfe1c32097ae13222deae55ae86c89c0bf5c41
RimDiversion <danchristensen9@gmail.com>
2022-01-04 15:31:37 -0500

    testing changes

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("pricipal").focus();
+    }
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     var total = principal + interest
