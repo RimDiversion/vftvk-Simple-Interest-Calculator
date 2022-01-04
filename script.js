9b44b76a4ea3f886b6795ed0aefd5121e32ebabf
RimDiversion <danchristensen9@gmail.com>
2022-01-04 15:02:29 -0500

    minor changes

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML= "If you deposit " + principal + ",/n"  
-        + "at an interest rate of " + rate + "%./n"
-        + "You will recieve an amount of " + (interest + principal) + ",/n"
-        + "in the year " + year   
+    document.getElementById("result2").innerHTML= "If you deposit " + principal + ",\n"  
+        + "at an interest rate of " + rate + "%.\n"
+        + "You will recieve an amount of " + (interest + principal) + ",\n"
+        + "in the year " + year;
+    document.getElementById("result").innerText= interest   
 }
         
 function updateRate()
